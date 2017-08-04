e0e3c4bc98103cf70764fc26e1b17675b165f92d
Younguk Kim <bluekyu.dev@gmail.com>
2017-08-04 13:45:40 +0900

    Update cmake files for debugging symbol

--- cmake/strip-debug-symbol.cmake
+++ cmake/strip-debug-symbol.cmake
@@ -1,10 +1,30 @@
 # Author: Younguk Kim (bluekyu)
 # Date  : 2017-07-28
 
-function(target_strip_debug_symbol target strip_postfix)
-    message(STATUS "Target '${target}' is set to strip debug symbol ...")
+# configure_debugging_information(target, [private_debinfo_postfix])
+#
+# Configure dubugging information for the target.
+#
+# If you want to strip private debugging information (ex, sources), you can set 'private_debinfo_postfix' string.
+# Then, you will get stripped debugging information and a full debugging file.
+function(configure_debugging_information target)
+    if(ARGV1)
+        set(private_debinfo_postfix ${ARGV1})
+        message(STATUS "Target '${target}' is set to strip debug symbol ...")
+    endif()
+
     foreach(config "Debug" "RelWithDebInfo")
         string(TOUPPER ${config} config_upper)
+
+        define_property(TARGET PROPERTY DEBINFO_PATH_${config_upper}
+            BRIEF_DOCS "The path of FULL debugging information file."
+            FULL_DOCS "The path of FULL debugging information file."
+        )
+        define_property(TARGET PROPERTY DEBINFO_STRIP_PATH_${config_upper}
+            BRIEF_DOCS "The path of STRIPPED debugging information file."
+            FULL_DOCS "The path of STRIPPED debugging information file."
+        )
+
         if(MSVC)
             get_target_property(LIBRARY_OUTPUT_DIRECTORY ${target} LIBRARY_OUTPUT_DIRECTORY_${config_upper})
             if(NOT LIBRARY_OUTPUT_DIRECTORY)
@@ -18,18 +38,47 @@ function(target_strip_debug_symbol target strip_postfix)
 
             get_target_property(POSTFIX ${target} ${config_upper}_POSTFIX)
 
-            set(STRIPPED_PDB_PATH "${LIBRARY_OUTPUT_DIRECTORY}/${OUTPUT_NAME}${POSTFIX}${strip_postfix}.pdb")
+            if(private_debinfo_postfix)
+                set(DEBINFO_PATH "${LIBRARY_OUTPUT_DIRECTORY}/${OUTPUT_NAME}${POSTFIX}${private_debinfo_postfix}.pdb")
+                set(DEBINFO_STRIP_PATH "${LIBRARY_OUTPUT_DIRECTORY}/${OUTPUT_NAME}${POSTFIX}.pdb")
 
-            # msvc
-            set_target_properties(${target} PROPERTIES LINK_FLAGS_${config_upper} /PDBSTRIPPED:${STRIPPED_PDB_PATH})
+                set_target_properties(${target} PROPERTIES DEBINFO_PATH_${config_upper} "${DEBINFO_PATH}")
+                set_target_properties(${target} PROPERTIES DEBINFO_STRIP_PATH_${config_upper} "${DEBINFO_STRIP_PATH}")
 
-            define_property(TARGET PROPERTY STRIPPED_PDB_PATH_${config_upper}
-                BRIEF_DOCS "The path of stripped PDB file."
-                FULL_DOCS "The path of stripped PDB file."
-            )
-            set_target_properties(${target} PROPERTIES STRIPPED_PDB_PATH_${config_upper} "${STRIPPED_PDB_PATH}")
+                set_target_properties(${target} PROPERTIES LINK_FLAGS_${config_upper}
+                    "/PDB:${DEBINFO_PATH} /PDBSTRIPPED:${DEBINFO_STRIP_PATH}"
+                )
+            else()
+                set(DEBINFO_PATH "${LIBRARY_OUTPUT_DIRECTORY}/${OUTPUT_NAME}${POSTFIX}.pdb")
+                set_target_properties(${target} PROPERTIES DEBINFO_PATH_${config_upper} "${DEBINFO_PATH}")
+            endif()
         else()
-            message(WARNING "Strip debug symbol is NOT working in this platform.")
+            if(strip_postfix)
+                message(WARNING "Striping private debugging infomation is NOT working in this platform.")
+            endif()
+            return()
+        endif()
+    endforeach()
+endfunction()
+
+# install_debugging_information(target)
+#
+# Install debugging information files for the target.
+function(install_debugging_information target)
+    foreach(config "Debug" "RelWithDebInfo")
+        if(MSVC)
+            string(TOUPPER ${config} config_upper)
+
+            get_target_property(DEBINFO_PATH ${target} DEBINFO_PATH_${config_upper})
+            get_target_property(DEBINFO_STRIP_PATH ${target} DEBINFO_STRIP_PATH_${config_upper})
+            if(NOT DEBINFO_PATH)
+                message(WARNING "'DEBINFO_PATH' variable is NOT found. Call 'configure_debug_info' function.")
+                return()
+            endif()
+            install(FILES "${DEBINFO_PATH}" DESTINATION "bin" CONFIGURATIONS ${config})
+            if(DEBINFO_STRIP_PATH)
+                install(FILES "${DEBINFO_STRIP_PATH}" DESTINATION "bin" CONFIGURATIONS ${config})
+            endif()
         endif()
     endforeach()
 endfunction()
