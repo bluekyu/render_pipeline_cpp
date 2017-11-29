73153f5c50fa0354a36d47f71b5de3fb0d2e4e4f
Younguk Kim <bluekyu.dev@gmail.com>
2017-11-29 10:40:32 +0900

    Fix cmake error if no refs/heads file

--- cmake/git-utils.cmake
+++ cmake/git-utils.cmake
@@ -14,9 +14,19 @@ function(get_git_head_hash project_directory out_hash_var)
         endif()
         file(STRINGS "${git_dir_path}/HEAD" git_HEAD_contents)
         string(REGEX MATCH "^[0-9a-f]+$" git_HEAD_hash ${git_HEAD_contents})
+        # it is ref, not hash
         if(NOT git_HEAD_hash)
             string(REGEX REPLACE "^ref: (.*)$" "\\1" git_HEAD_ref_path ${git_HEAD_contents})
-            file(STRINGS "${git_dir_path}/${git_HEAD_ref_path}" git_HEAD_hash)
+            # try to use file in refs/heads
+            if (EXISTS "${git_dir_path}/${git_HEAD_ref_path}")
+                file(STRINGS "${git_dir_path}/${git_HEAD_ref_path}" git_HEAD_hash REGEX "^[0-9a-f]+$")
+            # try to use info/refs file if refs/heads is clean (ex, by "git gc")
+            elseif(EXISTS "${git_dir_path}/info/refs")
+                file(STRINGS "${git_dir_path}/info/refs" git_info_ref_contents
+                    REGEX "[0-9a-f]+[ \t]+${git_HEAD_ref_path}$"
+                )
+                string(SUBSTRING "${git_info_ref_contents}" 0 40 git_HEAD_hash)
+            endif()
         endif()
 
         if(git_HEAD_hash)
